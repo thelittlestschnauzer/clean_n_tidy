2d6e6f9bc8dd4fa8869335b179ed50f727568059
github username <github email address>
2018-10-09 16:22:59 -0400

    generated user through devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+
+  root to: 'pages#home'
+
+  devise_scope :users do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :users do
+    get 'logout', to: 'devise/sessions#destroy'
+  end
+
+  devise_scope :users do
+    get 'signup', to: 'devise/registrations#new'
+  end 
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
